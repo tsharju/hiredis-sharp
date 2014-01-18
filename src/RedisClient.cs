1ce4705dacc2b4ade8025cbe73efa95c44e2af60
Teemu Harju <teemu@grandcrugames.com>
2014-01-18 13:38:44 +0200

    Removed RedisClient.Main method. You can find it under Example.cs.

--- src/RedisClient.cs
+++ src/RedisClient.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Hiredis
@@ -78,36 +77,6 @@ namespace Hiredis
 		public Reply PING()
 		{
 			return Command("PING");
-		}
-
-		static int Main(string[] args)
-		{
-			using (var client = new Client("localhost", 6379))
-			{
-				int operations = 100;
-
-				System.Console.WriteLine(String.Format("Performing {0} operations...", operations));
-
-				Stopwatch stopwatch = new Stopwatch();
-
-				stopwatch.Start();
-
-				for (int i=0; i < operations; i++) {
-					using (var reply = client.PING())
-					{
-						Console.WriteLine(reply.String);
-					}
-				}
-
-				stopwatch.Stop();
-
-				Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
-
-				var opsPerSec = operations / stopwatch.Elapsed.TotalSeconds;
-
-				Console.WriteLine("Operations per second: {0}", opsPerSec);
-			}
-			return 0;
 		}
 	}
 }
