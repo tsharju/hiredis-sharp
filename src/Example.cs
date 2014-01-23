1c7cdae6ef2b8ade41d1d772b0071be4e4a82f6a
Teemu Harju <teemu.harju@gmail.com>
2014-01-23 07:09:03 +0200

    Simply just use libhiredis API without trying to wrap it too much.

--- src/Example.cs
+++ src/Example.cs
@@ -9,60 +9,9 @@ public class HiredisExample
 		{
 			using (var client = new RedisClient("localhost", 6379))
 			{
-				using (var reply = client.SADD("set:test", "test"))
+				using (var reply = client.Command("SET %s %s", "test", "test"))
 				{
-					Console.WriteLine(reply.String);
-				}
-
-				using (var reply = client.SCARD("set:test"))
-				{
-					Console.WriteLine(reply.Integer);
-				}
-
-				using (var reply = client.SMEMBERS("set:test"))
-				{
-					int i = 0;
-					foreach (var r in reply.Array)
-					{
-						Console.WriteLine("INDEX: {0} VALUE: {1}", i, r.String);
-						i++;
-					}
-				}
-
-				string test;
-
-				using (var reply1 = client.APPEND("key:append", "Hello"))
-				using (var reply2 = client.APPEND("key:append", " World!"))
-				using (var reply3 = client.GET("key:append"))
-				{
-					Console.WriteLine("REPLY 1: {0}", reply1.Integer);
-					Console.WriteLine("REPLY 2: {0}", reply2.Integer);
-					Console.WriteLine("REPLY 3: {0}", reply3.String);
-					test = reply3.String;
-				}
-
-				Console.WriteLine(test);
-
-				using (var reply1 = client.SET("key:exists", "1"))
-				using (var reply2 = client.EXISTS("key:exists"))
-				using (var reply3 = client.EXISTS("key:noexists"))
-				{
-					Console.WriteLine("EXISTS: {0}", reply2.Integer);
-					Console.WriteLine("EXISTS: {0}", reply3.Integer);
-				}
-			}
-
-			using (var pipeline = new RedisPipelineClient("localhost", 6379))
-			{
-				for (int i=0; i < 10000; i++)
-				{
-					pipeline.SET(String.Format("pipeline:key{0}", i), "blaablaa");
-				}
-
-				for (int i=0; i < 10000; i++)
-				{
-					var reply = pipeline.RedisGetReply();
-					Console.WriteLine("{0}:{1}", i, reply.Integer);
+					Console.WriteLine("REPLY: {0}", reply.String);
 				}
 			}
 
--- src/Hiredis.cs
+++ src/Hiredis.cs
@@ -88,96 +88,40 @@ namespace Hiredis
 			this.contextPtr = IntPtr.Zero;
 		}
 
-		private RedisReply RedisCommand(string command)
+		public RedisReply Command(string command)
 		{
 			var replyPtr = LibHiredis.RedisCommand(this.contextPtr, command);
 			return new RedisReply(replyPtr);
 		}
 
-		private RedisReply RedisCommand(string command, string key)
+		public RedisReply Command(string command, string key)
 		{
 			var replyPtr = LibHiredis.RedisCommand(this.contextPtr, command, key);
 			return new RedisReply(replyPtr);
 		}
 
-		private RedisReply RedisCommand(string command, string key, string value)
+		public RedisReply Command(string command, string key, string value)
 		{
 			var replyPtr = LibHiredis.RedisCommand(this.contextPtr, command, key, value);
 			return new RedisReply(replyPtr);
 		}
 
-		public RedisReply APPEND(string key, string value)
-		{
-			return RedisCommand("APPEND %s %s", key, value);
-		}
-
-		public RedisReply EXISTS(string key)
-		{
-			return RedisCommand("EXISTS %s", key);
-		}
-
-		public RedisReply SET(string key, string value)
-		{
-			return RedisCommand("SET %s %s", key, value);
-		}
-
-		public RedisReply GET(string key)
-		{
-			return RedisCommand("GET %s", key);
-		}
-
-		public RedisReply DEL(string key)
-		{
-			return RedisCommand("DEL %s", key);
-		}
-
-		public RedisReply SADD(string key, string value)
-		{
-			return RedisCommand("SADD %s %s", key, value);
-		}
-
-		public RedisReply SMEMBERS(string key)
-		{
-
-			return RedisCommand("SMEMBERS %s", key);
-		}
-
-		public RedisReply SCARD(string key)
-		{
-			return RedisCommand("SCARD %s", key);
-		}
-
-		public RedisReply PING()
-		{
-			return RedisCommand("PING");
-		}
-	}
-
-	public class RedisPipelineClient : RedisClient
-	{
-		public RedisPipelineClient(string host, int port) : base(host, port) {}
-
-		private void RedisCommand(string command)
+		public void AppendCommand(string command)
 		{
 			LibHiredis.RedisAppendCommand(this.contextPtr, command);
 		}
 
-		private void RedisCommand(string command, string key)
+		public void AppendCommand(string command, string key)
 		{
 			LibHiredis.RedisAppendCommand(this.contextPtr, command, key);
 		}
 
-		private void RedisCommand(string command, string key, string value)
+		public void AppendCommand(string command, string key, string value)
 		{
 			LibHiredis.RedisAppendCommand(this.contextPtr, command, key, value);
 		}
 
-		public new void SET(string key, string value)
-		{
-			RedisCommand("SET %s %s", key, value);
-		}
-
-		public RedisReply RedisGetReply()
+		public RedisReply GetReply()
 		{
 			IntPtr replyPtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(ReplyStruct)));
 			Marshal.StructureToPtr(new ReplyStruct(), replyPtr, false);
