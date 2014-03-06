25863a1f42a613b884a586baf4c99f25e2784585
Teemu Harju <teemu@grandcrugames.com>
2014-03-06 15:46:45 +0200

    Make the library work on Windows .NET... sort of.

--- src/LibHiredis.cs
+++ src/LibHiredis.cs
@@ -31,7 +31,11 @@ namespace Hiredis
 		internal ReplyType type;
 		internal Int64 integer;
 		internal int len;
+#if __MonoCS__
 		[MarshalAs(UnmanagedType.LPTStr)]
+#else
+		[MarshalAs(UnmanagedType.LPStr)]
+#endif
 		internal string str;
 		internal Int64 elements;
 		internal IntPtr element;
@@ -39,40 +43,88 @@ namespace Hiredis
 
 	static internal class LibHiredis
 	{
-		[DllImport ("libhiredis", EntryPoint="redisFree")]
+#if __MonoCS__
+		[DllImport ("libhiredis", EntryPoint="redisFree", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisFree", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern void RedisFree(IntPtr context);
 
-		[DllImport ("libhiredis", EntryPoint="redisConnect")]
+#if __MonoCS__
+		[DllImport ("libhiredis", EntryPoint="redisConnect", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisConnect", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern IntPtr RedisConnect(string host, int port);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern IntPtr RedisCommand(IntPtr context, string command);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern IntPtr RedisCommand(IntPtr context, string command, string key);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern IntPtr RedisCommand(IntPtr context, string command, string key, string value);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern void RedisAppendCommand(IntPtr context, string command);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern void RedisAppendCommand(IntPtr context, string command, string key);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisAppendCommand", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern int RedisAppendCommand(IntPtr context, string command, string key, string value);
 
-		[DllImport ("libhiredis", EntryPoint="redisGetReply")]
+#if __MonoCS__
+		[DllImport ("libhiredis", EntryPoint="redisGetReply", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisGetReply", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern int RedisGetReply(IntPtr context, ref IntPtr reply);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisCommandArgv", CallingConvention=CallingConvention.Cdecl)]
-		internal static extern IntPtr RedisCommandArgv(IntPtr context, int argc, [In] string[] argv, [In] Int64[] argvlen);
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisCommandArgv", CallingConvention=CallingConvention.Cdecl)]
+#endif
+			internal static extern IntPtr RedisCommandArgv(IntPtr context, int argc, [In] string[] argv, [In] Int64[] argvlen);
 
+#if __MonoCS__
 		[DllImport ("libhiredis", EntryPoint="redisAppendCommandArgv", CallingConvention=CallingConvention.Cdecl)]
-		internal static extern int RedisAppendCommandArgv(IntPtr context, int argc, [In] string[] argv, [In] Int64[] argvlen);
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="redisAppendCommandArgv", CallingConvention=CallingConvention.Cdecl)]
+#endif
+			internal static extern int RedisAppendCommandArgv(IntPtr context, int argc, [In] string[] argv, [In] Int64[] argvlen);
 
-		[DllImport ("libhiredis", EntryPoint="freeReplyObject")]
+#if __MonoCS__
+		[DllImport ("libhiredis", EntryPoint="freeReplyObject", CallingConvention=CallingConvention.Cdecl)]
+#else
+		[DllImport("hiredis_win_x64.dll", EntryPoint="freeReplyObject", CallingConvention=CallingConvention.Cdecl)]
+#endif
 		internal static extern void FreeReplyObject(IntPtr reply);
 	}
 }
